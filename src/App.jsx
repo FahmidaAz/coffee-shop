9162213ac286821e8f750afa51b83ac30becd107
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2024-03-11 22:48:11 -0400

    file commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import BaristaForm from './Components/BaristaForm';
+
+function App() {
+  
+  return (
+    <>
+     <div className="title-container">
+          <h1 className='title'>On  my Grind</h1>
+          <p>So you think you can barista? Let's put that to the test...</p>
+          <BaristaForm/>
+     </div>
+    </>
+  )
+}
+
+export default App
